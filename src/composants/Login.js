68bcff7e9ad808a583df87a856b6422a12402867
Ibrahimch69 <96245990+Ibrahimch69@users.noreply.github.com>
2022-05-21 19:06:48 +0200

    gg

---
+++ src/composants/Login.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import axios from 'axios'
+import {createBrowserHistory} from 'history'
+import { useState } from 'react'
+ 
+const Login=()=>{
+ 
+  const [username,setUsername] = useState('');
+  const [pass,setPass] = useState('');
+  const[msg,setMsg] =useState('');
+  const [user,setUser] =useState({
+    email:'',
+    password:'',
+  });
+  const {email,password} = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  let history =createBrowserHistory();
+  const login_login =()=>{
+  const users = {user};
+  if(user.email === '')
+         {
+           alert('Email Field is empty')
+         }
+         else if(user.password === '')
+         {
+           alert('Pass Field is empty')
+         }
+  
+  
+  axios.post('http://127.0.0.1:8000/api/reactlogin',user)
+  .then(responce=>{
+    setMsg(responce.data);
+    localStorage.setItem("users",responce.data);
+    localStorage.setItem('users',responce.data);
+  history.push('/home')
+  
+  }) 
+  }
+  const flex={
+    display:'flex',
+    justifyContent :'center'
+     };
+     const form={
+        height:'300px',
+        width:'300px',
+        marginTop:"130px",
+        };
+ 
+    
+    return(
+      <div style={flex}>
+
+
+      <form style={form}>
+      
+      <h1>LOGIN</h1>
+      
+      
+      <br></br>
+      <label>Votre Email</label>
+      
+      <input  onChange={(e)=>onInputChange(e)} value={email} name="email" className="form-control input-sm" type="text" placeholder="Email" aria-label="Repository description" />
+      <br></br>
+      <label>mots de passe</label>
+      <input  onChange={(e)=>onInputChange(e)} name="password" value={password}  className="form-control input-sm" type="text" placeholder="Mots de passe" aria-label="Repository description" />
+      <br></br>
+      
+      <button  onClick={login_login} className="btn btn-dark" type="button">login</button>
+      
+      </form>
+      
+       </div>
+        
+    )
+} 
+
+export default Login
---
+++ src/composants/Singup.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+ 
+const Signup=()=>{
+    const flex={
+        display:'flex',
+        justifyContent :'center'
+         };
+         const form={
+           height:'300px',
+           width:'300px',
+           marginTop:"130px",
+            };
+
+            const [name,setNom] = useState('');
+  const [prenom,setPrenom] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+const profile = {name,email,password}; 
+const envoi =() =>{
+fetch('http://127.0.0.1:8000/api/register',
+{
+method:'POST',
+headers:{
+"Content-Type" : "application/json",
+"accept" : "application/json"},
+ body: JSON.stringify(profile)
+})
+.then(response => response.json)
+.then(rep =>{
+})
+
+}
+    return (
+        
+    <div style={flex}>
+    <form style={form}>
+    
+    <h1>INSCRIPTION</h1>
+    
+    
+    <label>Nom</label>
+    <input value={name}  onChange={(e)=>{setNom(e.target.value)}}  className="form-control input-sm" type="text" placeholder="NOM" aria-label="Repository description" />
+    <br></br>
+    <label>Prénom</label>
+    
+    <input value={prenom} onChange={(e)=>{setPrenom(e.target.value)}} className="form-control input-sm" type="text" placeholder="Prénom" aria-label="Repository description" />
+    
+    <br></br>
+    <label>Votre Email</label>
+    
+    <input value={email} onChange={(e)=>{setEmail(e.target.value)}} className="form-control input-sm" type="text" placeholder="Email" aria-label="Repository description" />
+    <br></br>
+    <label>Créé votre mots de passe</label>
+    
+    <input value={password} onChange={(e)=>{setPassword(e.target.value)}} className="form-control input-sm" type="text" placeholder="Mots de passe" aria-label="Repository description" />
+    <br></br>
+    
+    
+    
+    <Link to={'login'}>
+    <button onClick={envoi} className="btn btn-dark" type="button">S'inscrire</button>
+    </Link>
+    </form>
+    
+    
+    
+    
+    </div>
+    )
+}
+ 
+export default Signup
